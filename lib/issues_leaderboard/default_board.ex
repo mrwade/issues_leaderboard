c833adc416cd14f9065c6161f27f01bc2739bd3b
Kevin Wade <kevin@kevinwade.name>
2015-09-20 22:57:52 -0500

    Set after date and theme through env vars

--- lib/issues_leaderboard/default_board.ex
+++ lib/issues_leaderboard/default_board.ex
@@ -2,6 +2,9 @@ defmodule IssuesLeaderboard.DefaultBoard do
   use GenServer
 
   def start_link do
-    {:ok, _pid} = IssuesLeaderboard.Board.start_link("2014-10-10T00:00:00Z", "will smith")
+    {:ok, _pid} = IssuesLeaderboard.Board.start_link(
+      System.get_env("AFTER_DATE"),
+      System.get_env("THEME")
+    )
   end
 end
